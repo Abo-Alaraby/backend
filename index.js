3363efe74f70a7fc7cca56eda2fe657948df0e5d
yousefnegmeldin <yousefnegm@gmail.com>
2024-11-23 21:30:30 +0200

    reconfigured cors

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const app = express();
 
 const { login, signupUser } = require("./APIs/User-Service/auth");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
