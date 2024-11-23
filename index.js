59009221890994df7561da3f89ca7dede3b506f5
yousefnegmeldin <yousefnegm@gmail.com>
2024-11-23 21:18:18 +0200

    added cors import statement

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 
+const cors = require("cors");
+
 const Product = require("./Database/Models/product-model");
 
 const Cart = require("./Database/Models/cart-model");
